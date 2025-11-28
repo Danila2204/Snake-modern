f40565eed784023d8c5a22734ed4295e40bd007b
Danila <pogranichnik.marshal@yandex.ru>
2025-11-28 21:19:52 +0300

    Создан игровой цикл и добавлен моудль app.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,22 @@
 import { GameField } from "./GameField.js";
+import { GameLoop } from "./GameLoop.js";
 
 let context = document.querySelector("canvas").getContext("2d");
 
 let field = new GameField(context);
+
+class App {
+    constructor(context) {
+        this.context = context;
+
+        new GameLoop(this.update.bind(this), this.display.bind(this));
+    }
+
+    update(deltaTime) {
+    }
+
+    display() {
+    }
+}
+
+const app = new App(context);
